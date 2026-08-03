2ed7427d726cac5dd6fdd9869b325057e22df34c
Sebastien Foulon <contact.afflizen@gmail.com>
2026-08-03 11:42:47 +0200

    Ajouter la redirection Trade Republic

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        source: "/banques-en-ligne/trade-republic",
+        destination: "/investissement/trade-republic",
+        permanent: true,
+      },
+      {
         source: "/bons-plans/back-market",
         destination: "/e-commerce/back-market",
         permanent: true,
